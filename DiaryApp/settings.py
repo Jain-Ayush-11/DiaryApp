69cd560c64979eefda263c72f4c5f0553f877448
AlStien <aj601604@gmail.com>
2021-10-22 23:19:43 +0530

    added static files

--- DiaryApp/settings.py
+++ DiaryApp/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 
 # DataFlair Static Files Settings
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
-#DataFlair #User_Uploaded_Files
-MEDIA_URL = 'media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

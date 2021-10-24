efc692d0c9dc95d5e3daab3a207799c89590a639
AlStien <aj601604@gmail.com>
2021-10-24 19:11:28 +0530

    added bootstrap

--- DiaryApp/settings.py
+++ DiaryApp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # DataFlair Static Files Settings
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / '/static/'
 ]
 
 # Default primary key field type
